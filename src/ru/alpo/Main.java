9b98f349571c5c65cc889c4ed839ae05f6ab8047
178alpo <asponomar@gmail.com>
2019-07-30 12:41:41 +0300

    Void squareEquation method with global variables for equation roots

--- src/ru/alpo/Main.java
+++ src/ru/alpo/Main.java
@@ -1,24 +1,30 @@
 package ru.alpo;
 
 public class Main {
+    private static Double x1, x2;
+
     public static void main(String[] args) {
-        System.out.println(squareEquation(2, -5, 4) + "\n");
-        System.out.println(squareEquation(1,4,4));
+        squareEquation(2, -5, 4);
+        squareEquation(1, 4, 4);
+        squareEquation(2, -6, 3);
     }
 
-    public static double squareEquation(int a, int b, int c) {
-        System.out.println("The equation is: " + a + "x^2" + "+" + b + "x" + "+" + c + "=0");
+    public static void squareEquation(int a, int b, int c) {
+        System.out.printf("The equation is: %dx^2%+dx%+d=0 \n", a, b, c);
         double D = Math.pow(b, 2) - 4 * a * c;
-        System.out.println("D = " + D);
-        double x;
-        if (D >= 0) {
-            x = (-1 * b + Math.sqrt(D)) / (2 * a);
+        //System.out.print("D = " + D + ". ");
+
+        if (D > 0) {
+            x1 = (- b + Math.sqrt(D)) / (2 * a);
+            x2 = (- b - Math.sqrt(D)) / (2 * a);
+            System.out.println("There are two real roots here: \nX1=" + x1 + "\nX2=" + x2 + "\n");
+            //System.out.printf("(x%+.3f)*(x%+.3f)=0 \n\n", x1, x2);
+        } else if (D == 0) {
+            x1 = x2 = (double) - b / (2 * a);
+            System.out.println("There is one real root here: \nX =" + x1 + "\n");
+            //System.out.printf("(x%+.0f)^2=0 \n\n", x1, x2);
         } else {
-            System.out.println("There is no real x here");
-            x = 0;
+            System.out.println("There is no real roots here\n");
         }
-        return x;
-
-
     }
 }
