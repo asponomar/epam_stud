b52bb1f345a8a727932d74df35d18792166c825a
aponomar <asponomar@gmail.com>
2019-08-11 23:44:59 +0300

    Home work 5 init commit

---
+++ src/ru/alpo/homework_5/Library.java
@@ -0,0 +1,46 @@
+package ru.alpo.homework_5;
+
+import ru.alpo.homework_5.author.Author;
+import ru.alpo.homework_5.book.Book;
+
+public class Library {
+
+    public static void addAuthor(Author author) {
+        Storage.addAuthor(author);
+    }
+
+    public static void addBook(Book book) {
+        Storage.addBook(book);
+    }
+
+    public static void printAuthors() {
+        for (int i = 0; i < Storage.authors.length; i++) {
+            Author author = Storage.authors[i];
+            if (author != null) {
+                System.out.println(getAuthorAsStr(author));
+            }
+        }
+    }
+
+    public static void printBooks() {
+        for (int i = 0; i < Storage.books.length; i++) {
+            Book book = Storage.books[i];
+            if (book != null) {
+                System.out.println(getBookAsStr(book));
+            }
+        }
+    }
+
+    private static String getAuthorAsStr(Author author) {
+        return author.toString();
+    }
+
+    private static String getBookAsStr(Book book) {
+        return book.toString();
+    }
+
+    public static void printBooksInfo(Book book) {
+
+    }
+
+}
---
+++ src/ru/alpo/homework_5/LibraryDemo.java
@@ -0,0 +1,87 @@
+package ru.alpo.homework_5;
+
+import ru.alpo.homework_5.author.Author;
+import ru.alpo.homework_5.author.InputAuthor;
+import ru.alpo.homework_5.book.*;
+import static ru.alpo.homework_5.Library.*;
+
+public class LibraryDemo {
+    public static void main(String[] args) {
+        Library library = new Library();
+        initData(library);
+        printAuthors();
+        printBooks();
+    }
+
+    private static void initData(Library library) {
+        for (int i = 0; i < 0; i++) {
+            InputAuthor inputAuthor = new InputAuthor();
+            inputAuthor.setName("Alexander");
+            inputAuthor.setLastName("One more Pushkin");
+            inputAuthor.setBirthYear(1799);
+            inputAuthor.setDeathYear(1837);
+            Author author = valueOfAuthor(inputAuthor);
+            addAuthor(author);
+        }
+/*        System.out.println(Storage.authorIndex);
+        System.out.println(Storage.authors.length);*/
+
+
+        InputBook inputBook1 = new InputBook();
+        inputBook1.setName("Zolotaya rybka");
+        inputBook1.setPublishYear(2005);
+        inputBook1.setBookGenre(BookGenre.NOVEL);
+        Book book1 = valueOfForHandWrittenBook(inputBook1);
+
+        InputBook inputBook2 = new InputBook();
+        inputBook2.setName("Ruslan and Ludmila");
+        inputBook2.setPublishYear(2006);
+        inputBook2.setBookGenre(BookGenre.POEMS);
+        Book book2 = valueOfForPrintedBook(inputBook2);
+
+        InputAuthor inputAuthor1 = new InputAuthor();
+        inputAuthor1.setLastName("Pushkin");
+        inputAuthor1.setBirthYear(1799);
+        Author author = valueOfAuthor(inputAuthor1);
+        author.setBooks(new Book[]{book1, book2});
+
+        book1.setAuthors(new Author[]{author});
+        book2.setAuthors(new Author[]{author});
+
+        addBook(book1);
+        addBook(book2);
+        addAuthor(author);
+    }
+
+    private static Book valueOfForPrintedBook(InputBook inputBook) {
+        PrintedBook book = new PrintedBook();
+        book.setId(inputBook.getId());
+        book.setName(inputBook.getName());
+        book.setPublishYear(inputBook.getPublishYear());
+        book.setTotalPages(inputBook.getTotalPages());
+        book.setBookGenre(inputBook.getBookGenre());
+        book.setFontType(inputBook.getFontType());
+        return book;
+    }
+
+    private static Book valueOfForHandWrittenBook(InputBook inputBook) {
+        HandWrittenBook book = new HandWrittenBook();
+        book.setId(inputBook.getId());
+        book.setName(inputBook.getName());
+        book.setPublishYear(inputBook.getPublishYear());
+        book.setTotalPages(inputBook.getTotalPages());
+        book.setBookGenre(inputBook.getBookGenre());
+        book.setWritingInkType(inputBook.getWritingInkType());
+        return book;
+    }
+
+    private static Author valueOfAuthor(InputAuthor inputAuthor) {
+        Author author = new Author();
+        author.setId(inputAuthor.getId());
+        author.setName(inputAuthor.getName());
+        author.setLastName(inputAuthor.getLastName());
+        author.setBirthYear(inputAuthor.getBirthYear());
+        author.setDeathYear(inputAuthor.getDeathYear());
+        return author;
+    }
+}
